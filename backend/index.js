561635b8cc8dd6973a7bd64456b5348c618d4fd5
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-23 23:20:01 +0300

    Added organisation router

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userRouter from "./src/routes/user.routes.js";
+import organisationRouter from "./src/routes/organisation.routes.js"
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/api', userRouter);
+app.use('/api', organisationRouter);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to AKI API!')
+    res.send('Welcome to AKI API!');
 })
 
 app.listen(port, '0.0.0.0',() => {
