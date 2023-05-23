f2f4d2dd41a242c23b68a96a23bf8f96b1083dc5
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-23 23:20:26 +0300

    Updated PostgreSQL schema and config settings

--- backend/config/database.sql
+++ backend/config/database.sql
@@ -3,15 +3,40 @@
 -- Создание базы даннх
 CREATE DATABASE akidb;
 
+
+
+-- Настройка PostgreSQL на ECS Cloud, образ Ubuntu
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib
+
+-- Подключаеся к PostgreSQL с помощью пользователя postgres на сервере
+sudo -u postgres psql
+
+-- Создаем новую базу данных для нового пользователя 
+CREATE DATABASE akidb;
+
+-- Создаем нового пользователя и предоставляем роль
+CREATE USER akiuser WITH ENCRYPTED PASSWORD 'new_password';
+GRANT ALL ON DATABASE akidb TO akiuser;
+ALTER DATABASE akidb OWNER TO akiuser;
+GRANT USAGE, CREATE ON SCHEMA PUBLIC TO akiuser;
+
+-- Выход из базы данных
+exit
+
+-- Подключаеся к базе данных
+psql -h localhost -U new_username -d database_name
+
 -- установка модуля для генерации uuid 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- проверка установки модуля для генерации uuid 
 SELECT * FROM pg_extension WHERE extname = 'uuid-ossp';
 
--- Создание таблицы "Users"
+-- Создание таблицы "users"
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    full_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
@@ -22,53 +47,54 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Создание таблицы "SpaceOwners"
-CREATE TABLE spaceowners (
+-- Создание таблицы "organisations"
+CREATE TABLE organisations (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID,
-    position VARCHAR(255),
-    legal_entity_name VARCHAR(255),
-    inn VARCHAR(255),
-    rating FLOAT,
+    user_position VARCHAR(255) NOT NULL,
+    legal_entity_name VARCHAR(255) UNIQUE NOT NULL,
+    inn VARCHAR(255) UNIQUE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- Создание таблицы "Spaces"
+-- Создание таблицы "spaces"
 CREATE TABLE spaces (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID,
-    name VARCHAR(255),
-    industry VARCHAR(255),
-    description TEXT,
-    address VARCHAR(255),
+    space_name VARCHAR(255) NOT NULL,
+    industry VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    address VARCHAR(255) NOT NULL,
     operating_hours VARCHAR(255),
     renter_count INT,
-    price BIGINT,
+    price BIGINT NOT NULL,
     rating FLOAT,
     photos BYTEA[],
+    latitude FLOAT,
+    longitude FLOAT,
     verified BOOLEAN DEFAULT false,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- Создание таблицы "Services"
+-- Создание таблицы "services"
 CREATE TABLE services (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     space_id UUID,
-    name VARCHAR(255),
-    price BIGINT,
+    name VARCHAR(255) NOT NULL,
+    price BIGINT NOT NULL,
     FOREIGN KEY (space_id) REFERENCES spaces(id)
 );
 
--- Создание таблицы "Bookings"
+-- Создание таблицы "bookings"
 CREATE TABLE bookings (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     space_id UUID,
     renter_user_id UUID,
     owner_user_id UUID,
-    start_date DATE,
-    end_date DATE,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
     signed BOOLEAN DEFAULT false,
-    price BIGINT,
+    price BIGINT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (space_id) REFERENCES spaces(id),
@@ -76,15 +102,8 @@ CREATE TABLE bookings (
     FOREIGN KEY (owner_user_id) REFERENCES users(id)
 );
 
--- Подключаеся к базе данных
-psql -h localhost -U postgres -d akidb
+-- Выход из базы данных
+\q
 
--- Настройка PostgreSQL на ECS Cloud
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
-
--- Открыть PostgreSQL под пользователем postgres
-sudo su - postgres
-
--- Выходим из PostgreSQL
-\q exit
+-- Выход из PostgreSQL
+exit
