68db9a3eeac2cdb0cb3ba77eb1edad67243e345f
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-25 09:27:15 +0300

    Updated user routes with image endpoint

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 const router = new Router();
 import userController from "../controllers/user.controller.js";
+import { uploadProfilePicture } from "../middlewares/photoUpload.js";
 
 router.post('/user', userController.createUser);
 router.get('/users', userController.getUsers);
 router.get('/user/:id', userController.getOneUser);
 router.put('/user/:id', userController.updateUser);
+router.post('/user/:id/photo', uploadProfilePicture.single('profile_picture'), userController.uploadImage);
 router.delete('/user/:id', userController.deleteUser);
 
 export default router;
