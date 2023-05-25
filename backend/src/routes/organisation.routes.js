60f42b423d8f38c27bc2ad5a5b80ab495e84cfcb
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-25 09:26:48 +0300

    Added organisation routes

--- backend/src/routes/organisation.routes.js
+++ backend/src/routes/organisation.routes.js
@@ -3,5 +3,10 @@ const router = new Router();
 import organisationController from "../controllers/organisation.controller.js";
 
 router.post('/organisation', organisationController.createOrganisation);
+router.get('/organisation/:id', organisationController.getOneOrganisation);
+router.get('/organisations', organisationController.getAllOrganisations);
+router.put('/organisation/:id', organisationController.updateOrganisation);
+router.delete('/organisation/:id', organisationController.deleteOrganisation);
+
 
 export default router;
