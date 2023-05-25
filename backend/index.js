1b544a28aebc5e8aeda0f2eb229a99c9d0d834ac
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-25 09:24:26 +0300

    Added space router

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import bodyParser from "body-parser";
 import userRouter from "./src/routes/user.routes.js";
-import organisationRouter from "./src/routes/organisation.routes.js"
+import organisationRouter from "./src/routes/organisation.routes.js";
+import spaceRouter from "./src/routes/space.routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,8 +10,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 
+app.use('/images',express.static('images'));
+
 app.use('/api', userRouter);
 app.use('/api', organisationRouter);
+app.use('/api', spaceRouter);
 
 app.get('/', (req, res) => {
     res.send('Welcome to AKI API!');
