6ecfd77bea3db4e1afb6a2f658b49132990f609a
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-27 18:44:50 +0300

    Updated user routes with login path

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 const router = new Router();
 import userController from "../controllers/user.controller.js";
 import { uploadProfilePicture } from "../middlewares/photoUpload.js";
+import jwtAuthMiddleware from "../middlewares/jwtAuthMiddleware.js";
 
 router.post('/user', userController.createUser);
-router.get('/users', userController.getUsers);
-router.get('/user/:id', userController.getOneUser);
-router.put('/user/:id', userController.updateUser);
-router.post('/user/:id/photo', uploadProfilePicture.single('profile_picture'), userController.uploadImage);
-router.delete('/user/:id', userController.deleteUser);
+router.post('/user/login', userController.login);
+router.get('/users', jwtAuthMiddleware, userController.getUsers);
+router.get('/user/:id', jwtAuthMiddleware, userController.getOneUser);
+router.put('/user/:id', jwtAuthMiddleware, userController.updateUser);
+router.post('/user/:id/photo', jwtAuthMiddleware, uploadProfilePicture.single('profile_picture'), userController.uploadImage);
+router.delete('/user/:id', jwtAuthMiddleware, userController.deleteUser);
 
 export default router;
