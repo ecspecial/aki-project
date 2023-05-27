0fbe49a5dfacf7ace5e361e558be7ddf8d526419
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-05-27 18:43:18 +0300

    Added jwt config file

---
+++ backend/config/jwtConfig.js
@@ -0,0 +1,6 @@
+const jwtSecret = "your_jwt_secret_key";
+const jwtExpiresIn = "7d";
+const jwtAlgorithm = "HS256";
+const bcryptSaltRounds = 10;
+
+export { jwtSecret, jwtExpiresIn, jwtAlgorithm, bcryptSaltRounds };
