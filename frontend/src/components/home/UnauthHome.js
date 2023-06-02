20aea8b3d2a232e192d113718a202e89caeab712
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-06-02 19:06:18 +0300

    Added basic welcome screen component

---
+++ frontend/src/components/home/UnauthHome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function UnauthHome() {
+  return (
+    <div>
+      <main>
+        <h1>Welcome to the Creative Space Listings</h1>
+        {/* Add your creative space listings here */}
+      </main>
+    </div>
+  );
+}
+
+export default UnauthHome;
