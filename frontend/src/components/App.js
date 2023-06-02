742f135e1a5769c3ccb940bd4766cd9cc90d1380
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-06-02 19:04:19 +0300

    Set up App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './common/navigation/NavigationBar';
+import UnauthHome from './home/UnauthHome';
+import LoginComponent from './auth/LoginComponent';
+import Footer from './common/footer/PageFooter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome
-        </p>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route 
+            path='/'
+            element={<UnauthHome />}
+          />
+          <Route 
+            path='/login'
+            element={<LoginComponent />}
+          />
+        </Routes>
+        
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
